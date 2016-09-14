722cebee81703f4334ebf48c7c492810806db0eb
valerieshoskes <indi.vs@gmail.com>
2016-09-14 16:06:25 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoskesDoubleAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int userNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Double(userNumber));
+        }
+        static int Double(int userNumber)
+        {
+            return userNumber + userNumber;
+        }
+
+    }
+}
